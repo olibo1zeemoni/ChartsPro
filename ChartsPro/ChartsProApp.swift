9b8a20e4c5e9967af286599ca89ee86cc3993c0f
George Boahene <39881594+olibo1zeemoni@users.noreply.github.com>
2024-08-08 12:48:43 +0000

    initial commit

---
+++ ChartsPro/ChartsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsProApp.swift
+//  ChartsPro
+//
+//  Created by Olibo moni on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChartsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
