60ef5173a6b2d97e91ee2d8a33a9e8d11dc10616
George Boahene <39881594+olibo1zeemoni@users.noreply.github.com>
2024-08-24 04:02:36 +0000

    pie chart and donut

--- ChartsPro/Views/book category stats/SalesPerBookCategoryPieChartView.swift
+++ ChartsPro/Views/book category stats/SalesPerBookCategoryPieChartView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import Charts
 
 struct SalesPerBookCategoryPieChartView: View {
+    @ObservedObject var salesViewModel: SalesViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(salesViewModel.totalSalesPerCategory, id: \.category) { data in
+            SectorMark(angle: .value("Book Category", data.sales), innerRadius: .ratio(0.618), angularInset: 1.5)
+                .cornerRadius(15)
+                .opacity(salesViewModel.bestSellingCategory?.category == data.category ? 1 : 0.3)
+                .foregroundStyle(by: .value("Category", data.category.displayName))
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .chartLegend(position: .bottom, spacing: 20)
+        .chartBackground { chartProxy in
+            GeometryReader{ geometry in
+                let frame = geometry[chartProxy.plotFrame!]
+                let visibleSize = min(frame.width, frame.height)
+                
+                if let bestSellingCategory = salesViewModel.bestSellingCategory {
+                    VStack {
+                        Text("Most sold category")
+                            .font(.callout)
+                            .foregroundStyle(.secondary)
+                        Text(bestSellingCategory.category.displayName)
+                            .font(.title2.bold())
+                            .foregroundStyle(.primary)
+                        Text(bestSellingCategory.sales.formatted() + " sold")
+                            .font(.callout)
+                            .foregroundStyle(.secondary)
+                    }
+                    .frame(maxWidth: visibleSize * 0.618, maxHeight: visibleSize * 0.618)
+                    .position(x: frame.midX, y: frame.midY)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SalesPerBookCategoryPieChartView()
+    SalesPerBookCategoryPieChartView(salesViewModel: .preview)
+        .padding(10)
 }
--- ChartsPro/Views/book category stats/SimpleSalesPerBookCategoryView.swift
+++ ChartsPro/Views/book category stats/SimpleSalesPerBookCategoryView.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import Charts
 
 struct SimpleSalesPerBookCategoryView: View {
+    @ObservedObject var salesViewModel: SalesViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            SalesPerBookCategoryHeaderView(salesViewModel: salesViewModel)
+            
+            Chart(salesViewModel.totalSalesPerCategory, id: \.category) { data in
+                SectorMark(angle: .value("Book Category", data.sales), innerRadius: .ratio(0.618), angularInset: 1.5)
+                    .cornerRadius(15)
+                    .opacity(salesViewModel.bestSellingCategory?.category == data.category ? 1 : 0.3)
+
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .frame(height: 70)
+        }
     }
 }
 
 #Preview {
-    SimpleSalesPerBookCategoryView()
+    SimpleSalesPerBookCategoryView(salesViewModel: .preview)
 }
